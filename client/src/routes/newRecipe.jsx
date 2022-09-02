b8950ee996d40da084d35212e42a19d6db517411
Danek <danek.fill@outlook.com>
2022-09-02 16:45:21 -0400

    Add skeleton for new recipe/fork recipe

--- client/src/routes/newRecipe.jsx
+++ client/src/routes/newRecipe.jsx
@@ -1,5 +1,22 @@
-export default function NewRecipe() {
+export default function NewRecipe(props) {
+  
+  
   return (
-    <header><h1>Create a new recipe below!</h1></header>
+    <header>
+    <h1>Create a new recipe below!</h1>
+
+    <h2>Ingredients</h2>
+      <p>+Add Component and logic</p>
+      props.ingredients.map here, creating list item for each prop passed
+      
+    <h2>Equipment</h2>
+    <p>+Add Component and logic</p>
+      props.equipment.map here, creating list item for each prop passed
+    
+    <h2>Instructions</h2>
+      <p>+Add Component and logic</p>
+      props.instructions.map here, creating list item for each prop passed
+
+    </header>
   );  
 }
