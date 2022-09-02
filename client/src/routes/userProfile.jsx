1a04fe5245070db5f4f7de38dddd5d52ac08d359
Danek <danek.fill@outlook.com>
2022-09-02 18:37:59 -0400

    Remove placeholder logo

--- client/src/routes/userProfile.jsx
+++ client/src/routes/userProfile.jsx
@@ -1,9 +1,10 @@
 import RecipeList from './recipes/recipeList'
 import {} from "../helpers/profileHelpers";
+import { Link } from "react-router-dom";
 
 export default function UserProfile() {
   return (
-    
+
     <header>
     <h1>Username</h1>
     <p>The recipe sideways scoll item using recipe cards</p>
@@ -15,6 +16,10 @@ export default function UserProfile() {
     <p>Socials goes here</p>
     <p>Qualifications go here</p>
     <p>Badges go here</p>
+
+    
+    <Link to='/users/:userName/:recipeId'>TEMPORARY LINK TO A RECIPE</Link>
+
     </header>
     
   );  
