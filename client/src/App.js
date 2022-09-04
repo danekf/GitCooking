b3ba1697b5c5965f4549dfe2db7dc0c46ebe20db
KayliedeBlois <kayliedeblois@gmail.com>
2022-09-04 19:09:07 -0400

    Update Register and Create a New Recipe badges

--- client/src/App.js
+++ client/src/App.js
@@ -18,13 +18,16 @@ function App() {
       <Link to='/'><img src='/GitCookingLogo.png' width = '50px' alt="gitCookingLogo" className="gitcooking-title"></img>GitCooking</Link>
       {/* Condiditional render here for what to show */}
       {/* Not logged in stuff */}
-      <Link to='/register'>Register</Link>
-      <Link to='/login'>Login</Link>
+
+      <Link to='/register' className="badge badge-secondary">Register</Link>
+      {/* Add a redirect link on Register page for those who have a login */}
+      {/* <Link to='/login'>Login</Link> */}
 
       {/* Logged in stuff here */}
+      <Link to='/newRecipe' className="badge badge-secondary">Create a new recipe</Link>
       <img src = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Small-dark-green-circle.svg/1200px-Small-dark-green-circle.svg.png" width = "50px" alt='profilePic'></img>
       {/* Implement "click to edit" on profile pick */}
-      <Link to='/newRecipe'>Create a new recipe</Link>
+
     </header>
     
     <body>
@@ -34,6 +37,8 @@ function App() {
     <footer class="app-footer">
       <nav class="app-footer-nav">
           <li><Link to="/users/:userName"><i class="fa-solid fa-user app-footer-nav-list-item"></i></Link></li>
+                {/* Add Login for conditional rendering if someone clicks on Profile in footernav & is not logged in */}
+                {/* <Link to='/login'>Login</Link> */}
           <li><Link to="/favourites"><i class="fa-solid fa-star app-footer-nav-list-item"></i></Link></li>
           <li><Link to="/chat"><i class="fa-solid fa-comment app-footer-nav-list-item"></i></Link></li>
           <li><Link to="/menu"><i class="fa-solid fa-bars app-footer-nav-list-item"></i></Link></li>
