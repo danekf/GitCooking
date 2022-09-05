a6ec114ab9f70dede15f867e6f32d8b3b33044d0
Danek <danek.fill@outlook.com>
2022-09-05 17:30:57 -0400

    Add cors

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,7 @@
 const ENV = require("dotenv").config();
 const PORT = process.env.PORT || 8001;
 const express = require('express');
-
+const cors = require('cors');
 
 //PG connection and database setup
 const { Pool } = require('pg');
@@ -17,6 +17,9 @@ const db = new Pool(dbParams);
 db.connect()
   .then(()=> console.log(`Connected to ${dbParams.database}`))
   .catch(error => console.log(`Error connecting to database. ${error}`));
+
+//cors setup
+app.use(cors());
 
 //main application folder once connection setup
 const app = express();
