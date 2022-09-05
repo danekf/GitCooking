fcffb6846e4a19c128e4f54828a3e77ebb4896aa
Mardin Hener <mardinhener14@gmail.com>
2022-09-05 15:07:23 -0400

    Complete mobile view for create a new recipe card

--- client/src/routes/recipes/newRecipe.jsx
+++ client/src/routes/recipes/newRecipe.jsx
@@ -3,40 +3,40 @@ import './styles.scss'
 export default function NewRecipe(props) {
   return (
     <>
-      <div>
+      <div className='create-a-new-recipe-card'>
         <form action="">
 
-          <h1>Create a New Recipe</h1>
+          <h1 className='recipe-title'>Create a New Recipe</h1>
 
-          <h4>Recipe Title</h4>
+          <h4>Recipe Title:</h4>
           <input type="text" name="recipe-title" id="recipe-title" />
 
-          <h4>Prep Time</h4>
-          <input type="text" name="prep-time" id="prep-time" />
+          <h4>Prep Time:</h4>
+          <input className='prep-time' type="text" name="prep-time" id="prep-time" />
+          
+          <h4>Cooking Time:</h4>
+          <input className='cooking-time' type="text" name="cooking-time" id="cooking-time" />
+          
+          <h4>Servings:</h4>
+          <input className='servings' type="text" name="servings" id="servings" />
 
-          <h4>Cooking Time</h4>
-          <input type="text" name="cooking-time" id="cooking-time" />
-
-          <h4>Servings</h4>
-          <input type="text" name="servings" id="servings" />
-
-          <h4>Ingredients</h4>
+          <h4>Ingredients:</h4>
           <input type="list" name="ingredients" id="ingredients" />
 
-          <h4>Equipments Required</h4>
+          <h4>Equipments Required:</h4>
           <input type="list" name="equipments" id="equipments" />
 
-          <h4>Instructions</h4>
+          <h4>Instructions:</h4>
           <input type="list" name="instructions" id="instructions" />
           
-          <h4>Tags</h4>
+          <h4>Tags:</h4>
           <input type="text" name="tags" id="tags" />
           
-          <h4>Upload an Image</h4>
-          <input type="file" name="image-upload" id="image-upload" />
+          <h4>Upload an Image:</h4>
+          <input className='recipe-btn-upload' type="file" name="image-upload" id="image-upload" />
 
-          <button type="submit">Submit Recipe!</button>
-          <button type="reset">Reset Recipe Form</button>
+          <button className='recipe-btn-submit' type="submit">Submit Recipe!</button>
+          <button className='recipe-btn-reset' type="reset">Reset Recipe Form</button>
 
         </form>
       </div>
