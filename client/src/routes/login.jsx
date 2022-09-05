1659b39910ad7e0305ec693d44d9c4546d28597d
KayliedeBlois <kayliedeblois@gmail.com>
2022-09-04 20:24:58 -0400

    Update Login title styling

--- client/src/routes/login.jsx
+++ client/src/routes/login.jsx
@@ -3,7 +3,7 @@ import './login.scss';
 export default function Login() {
   return (
     <body>
-      <h1>Login</h1>
+      <h1 className="login-title">Login</h1>
       <form className="login-form">
         <input type='text' id='username' name='username' placeholder="username"></input>
         <input type='password' id='password' name='password' placeholder='password'></input>
