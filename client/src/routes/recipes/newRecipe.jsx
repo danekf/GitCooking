8e31f7841167460bb2be72668e4ad76baf7eb233
Mardin Hener <mardinhener14@gmail.com>
2022-09-05 13:57:28 -0400

    Add Create a New Recipe Form

--- client/src/routes/recipes/newRecipe.jsx
+++ client/src/routes/recipes/newRecipe.jsx
@@ -1,23 +1,45 @@
+import './styles.scss'
+
 export default function NewRecipe(props) {
-  
-  
   return (
-    <header>
-    <h1>Create a new recipe below!</h1>
+    <>
+      <div>
+        <form action="">
 
-    <h2>Ingredients</h2>
-      <p>+Add Component and logic</p>
-      props.ingredients.map here, creating list item for each prop passed
-      
-    <h2>Equipment</h2>
-    <p>+Add Component and logic</p>
-      props.equipment.map here, creating list item for each prop passed
-    
-    <h2>Instructions</h2>
-      <p>+Add Component and logic</p>
-      props.instructions.map here, creating list item for each prop passed
+          <h1>Create a New Recipe</h1>
 
-    <h2>Tags placeholder</h2>
-    </header>
+          <h4>Recipe Title</h4>
+          <input type="text" name="recipe-title" id="recipe-title" />
+
+          <h4>Prep Time</h4>
+          <input type="text" name="prep-time" id="prep-time" />
+
+          <h4>Cooking Time</h4>
+          <input type="text" name="cooking-time" id="cooking-time" />
+
+          <h4>Servings</h4>
+          <input type="text" name="servings" id="servings" />
+
+          <h4>Ingredients</h4>
+          <input type="list" name="ingredients" id="ingredients" />
+
+          <h4>Equipments Required</h4>
+          <input type="list" name="equipments" id="equipments" />
+
+          <h4>Instructions</h4>
+          <input type="list" name="instructions" id="instructions" />
+          
+          <h4>Tags</h4>
+          <input type="text" name="tags" id="tags" />
+          
+          <h4>Upload an Image</h4>
+          <input type="file" name="image-upload" id="image-upload" />
+
+          <button type="submit">Submit Recipe!</button>
+          <button type="reset">Reset Recipe Form</button>
+
+        </form>
+      </div>
+    </>
   );  
 }
