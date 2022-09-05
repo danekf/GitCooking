2295439c02b996654af325d673ddf633c89ba0bf
Danek <danek.fill@outlook.com>
2022-09-05 10:39:28 -0400

    Commit so i can switch branches

--- server/src/db/seeds/users.js
+++ server/src/db/seeds/users.js
@@ -4,7 +4,7 @@ VALUES
 'Kingsleigh',
 'alice@wonderland.ca',
 'HeartChef',
-'$2a$10$wkx0GNQ9p3yBz0psMlV/vuIRT6UFUL2bWbqyQ1caUZNicqXu24Mae',
+'password',
 'Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, "and what is the use of a book," thought Alice "without pictures or conversation?"',
 'alice@chefland.com',
 ARRAY ['Facebook','Insta','Tokker'],
