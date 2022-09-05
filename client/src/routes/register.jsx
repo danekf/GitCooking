1104fdbf96cb4047f86771ae9dc1bc84f85f2efb
KayliedeBlois <kayliedeblois@gmail.com>
2022-09-04 21:07:01 -0400

    Add form styling

--- client/src/routes/register.jsx
+++ client/src/routes/register.jsx
@@ -7,21 +7,22 @@ export default function Register() {
         <h1 className="register-title">Register</h1>
         <h2 className="register-to-login">Already registered?</h2>
         <Link to='/login' className="badge badge-primary">Login</Link>
-        <form>
+        <form className="register-form">
           <input className="register-form-item" type='text' id='email' name='email' placeholder="Email"></input>
           <input className="register-form-item" type='text' id='firstname' name='first name' placeholder='First Name'></input>
           <input className="register-form-item" type='text' id='lastname' name='last name' placeholder='Last Name'></input>
           <input className="register-form-item" type='text' id='username' name='username' placeholder='Username'></input>
           <input className="register-form-item" type='password' id='password' name='password' placeholder='Password'></input>
 
-          <label for="profile picture">Upload a photo:</label>
+          <label for="profile picture">Upload a Profile Picture:</label>
           <input 
+            className="register-form-item"
             type='file' 
             id='profile picture' 
             name='profile picture' 
-            placeholder='Upload Profile Picture'
-            // onChange={onChange}
+            // // onChange={onChange}
           ></input>
+          
           <button className="badge badge-primary" >Register</button>
         </form>
     </body>
