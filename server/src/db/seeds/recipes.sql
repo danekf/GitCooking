8e7427c454d38765325daf56b2b08247e1e948cc
Danek <danek.fill@outlook.com>
2022-09-05 12:58:09 -0400

    Fix recipe seed JSON

--- server/src/db/seeds/recipes.sql
+++ server/src/db/seeds/recipes.sql
@@ -5,15 +5,15 @@ VALUES(
   '[{
     "pasta": "1 handful",
     "pesto": "3TBsp",
-    "salt": "Enough, not too little"}
-  ]',
+    "salt": "Enough, not too little"
+  }]',
   '[{"pot": 1}]',
   '[
-    {estimated time: 1, instruction: "Add salt to water so that water tastes distinctly salted, This is to flavour the pasta, NOT to make the water boil faster. It is essential to tasty pasta.}
-    {estimate time: 5, instruction: "Boil water"},
-    {estimated time: 7, instruction: "Cook the pasta in the water"},
-    {estimated time: 2, instruction: "Drain Pasta and add pesto."},
-    {estimated time: 1, instruction: "Serve."}
+    {"estimated time": 1, "instruction": "Add salt to water so that water tastes distinctly salted, This is to flavour the pasta, NOT to make the water boil faster. It is essential to tasty pasta.},
+    {"estimated time": 5, "instruction": "Boil water"},
+    {"estimated time": 7, "instruction": "Cook the pasta in the water"},
+    {"estimated time": 2, "instruction": "Drain Pasta and add pesto."},
+    {"estimated time": 1, "instruction": "Serve."}
   ]',
   'No Photos yet',
   ARRAY ['Pasta', 'Simple', 'Quick', 'Easy', 'Budget'],
@@ -30,16 +30,16 @@ VALUES(
     "pasta": "1 handful",
     "Tomato Sauce": "3TBsp",
     "salt": "Enough, not too little",
-    "Meatballs": 5}
-  ]',
+    "Meatballs": 5
+  }]',
   '[{"pot": 1}]',
   '[
-    {estimated time: 1, instruction: "Add salt to water so that water tastes distinctly salted, This is to flavour the pasta, NOT to make the water boil faster. It is essential to tasty pasta.}
-    {estimate time: 5, instruction: "Boil water"},
-    {estimated time: 7, instruction: "Cook the pasta in the water"},
-    {estimated time: 7, instruction: "While pasta is cooking, heat up pasta sauce and add in meatballs to heat through. "},
-    {estimated time: 2, instruction: "Drain Pasta and add sauce + meatballs."},
-    {estimated time: 1, instruction: "Serve."}
+    {"estimated time": 1, "instruction": "Add salt to water so that water tastes distinctly salted, This is to flavour the pasta, NOT to make the water boil faster. It is essential to tasty pasta.},
+    {"estimated time": 5, "instruction": "Boil water"},
+    {"estimated time": 7, "instruction": "Cook the pasta in the water"},
+    {"estimated time": 7, "instruction": "While pasta is cooking, heat up pasta sauce and add in meatballs to heat through."},
+    {"estimated time": 2, "instruction": "Drain Pasta and add sauce + meatballs."},
+    {"estimated time": 1, "instruction": "Serve."}
   ]',
   'No Photos yet',
   ARRAY ['Pasta', 'Simple', 'Quick', 'Easy', 'Budget', 'Meatballs', 'Tomato Sauce'],
