81e09c8daccd3e042cc49140acb35e1b18c336f2
Danek <danek.fill@outlook.com>
2022-09-05 15:32:48 -0400

    skeleton for login created

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -3,9 +3,47 @@ const router = express.Router();
 
 module.exports = (db) => {
   
+  //it is a get request because they are trying to log in
   router.get('/login', (request, response)=>{
-    console.log('Users login requested');
+    //from client request
+    const username = req.body.username;
+    const password_hash = req.body.password;
+
+    //////delete me, testing logs/////
+    console.log(`Username: ${username}, password: ${password_hash}`)
+    //////////////////////////////////
+
+    //search string
+    const queryString = `
+    SELECT *
+    FROM users
+    WHERE email = $1
+    OR username = $1`
+
+    const queryValues = [`${username}`];
+
+    db.query(queryString, queryValues)
+      .then(({rows: users}) => {
+        //bcrypt the password on the client side BEFORE SENDING to server. Server should not handle the password as far as I know. Must confirm.
+
+        
+        if (!users[0] ){
+          response.json({Error: "Username or email not found."})
+
+        } else if(password_hash != users[0].password_hash) {
+          response.json({Error: "Password is incorrect."})
+
+        } else{
+          response.json(users[0])
+        }
+      })
+
+      .catch(error => {
+
+      });
+
   });
+
 
 
 
