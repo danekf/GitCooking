f5467314f327eb02805ab794861243a8250e88bf
Mardin Hener <mardinhener14@gmail.com>
2022-09-05 17:37:00 -0400

    Fix viewport for mobile view of user profile

--- client/src/routes/profile_components/badges.jsx
+++ client/src/routes/profile_components/badges.jsx
@@ -3,6 +3,18 @@ export default function Badges () {
     <>
       <div>
         <p className="badges">Badges</p>
+        <ul>
+          <li>
+            <i class="fa-solid fa-bowl-rice"></i>
+          </li>
+          <li>
+          <i class="fa-solid fa-cookie-bite"></i>
+          </li>
+          <li>
+          <i class="fa-solid fa-burger"></i>
+          </li>
+
+        </ul>
       </div>
     </>
   );
