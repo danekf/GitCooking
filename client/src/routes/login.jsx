ee39a06a13a7d9071e71f2dfacedf83f6a5c3837
Danek <danek.fill@outlook.com>
2022-09-05 16:22:32 -0400

    Login progress and various small react fixes

--- client/src/routes/login.jsx
+++ client/src/routes/login.jsx
@@ -1,25 +1,52 @@
 import './login.scss';
-import axios from "axios";
+import { React, useState } from 'react';
+import password_hasher_bcrypt from "../helpers/password_hasher_bcrypt";
+import axios from 'axios';
+
 
 export default function Login() {
 
-  function login(){
-    console.log('Attempting Login');
-    ///axios will get from proxy
-    axios.get('/login')
-      .then((data) => {
-        console.log(data);
-      })
-      .catch(error => console.log(error));
-  };
+
+  const [formValue, setformValue] = useState({
+    username: '',
+    password: ''
+  })
+
+  const submitLogin = (event) => {
+    console.log('Attempting Login')
+    //create body form data prior to submitting
+    const loginData = new FormData();
+
+    //add username to this form data
+    loginData.append('username', formValue.username);
+
+    //when we add bcrypt, we must HASH the password prior to sending it. Currently we do not hash.
+    //add password to form data
+    loginData.append('password_hash')
+
+    //make request with form data
+    axios({
+      method: 'post',
+      url: '/login',
+      data: loginData
+    });
+
+  }
+
+  const handleChange =(event) =>{
+    setformValue({
+      ...formValue,
+      [event.target.name]: event.target.value
+    });
+  }
 
   return (
     <body>
       <h1 className="login-title">Login</h1>
-      <form className="login-form" onSubmit={login}>
-        <input className="login-form-item" type='text' id='username' name='username' placeholder="username"></input>
-        <input className="login-form-item" type='password' id='password' name='password' placeholder='password'></input>
-        <button className="badge badge-primary" >Login</button>
+      <form className="login-form" onSubmit={submitLogin}>
+        <input className="login-form-item" type='text' id='username' name='username' placeholder='username' value={formValue.username} onChange={handleChange}></input>
+        <input className="login-form-item" type='password' id='password' name='password' placeholder='password' value={formValue.password} onChange={handleChange}></input>
+        <button className="badge badge-primary" type='submit'>Login</button>
       </form>
     </body>
 
