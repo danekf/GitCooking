687209cca2f1bdb5c0beeba2ab9d5c6c3fbfcacb
KayliedeBlois <kayliedeblois@gmail.com>
2022-09-04 20:33:52 -0400

    Add form spacing

--- client/src/routes/login.jsx
+++ client/src/routes/login.jsx
@@ -5,8 +5,8 @@ export default function Login() {
     <body>
       <h1 className="login-title">Login</h1>
       <form className="login-form">
-        <input type='text' id='username' name='username' placeholder="username"></input>
-        <input type='password' id='password' name='password' placeholder='password'></input>
+        <input className="login-form-item" type='text' id='username' name='username' placeholder="username"></input>
+        <input className="login-form-item" type='password' id='password' name='password' placeholder='password'></input>
         <button className="badge badge-primary">Login</button>
       </form>
     </body>
