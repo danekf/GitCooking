e5fc457201178d34e34c24ba71605f7bbf658de1
Mardin Hener <mardinhener14@gmail.com>
2022-09-06 00:20:45 -0400

    Edit Register page for styling consistency

--- client/src/routes/register.jsx
+++ client/src/routes/register.jsx
@@ -4,27 +4,24 @@ import './register.scss';
 export default function Register() {
   return (
       <body>
+        <div className="register-card">
         <h1 className="register-title">Register</h1>
-        <h2 className="register-to-login">Already registered?</h2>
-        <Link to='/login' className="badge badge-primary">Login</Link>
+
+        <h6 className="register-to-login">Already registered?</h6>
+        <Link to='/login' className="login-btn">Login</Link>
+
         <form className="register-form">
-          <input className="register-form-item" type='text' id='email' name='email' placeholder="Email"></input>
           <input className="register-form-item" type='text' id='first-name' name='first name' placeholder='First Name'></input>
           <input className="register-form-item" type='text' id='last-name' name='last name' placeholder='Last Name'></input>
+          <input className="register-form-item" type='email' id='email' name='email' placeholder="Email"></input>
           <input className="register-form-item" type='text' id='username' name='username' placeholder='Username'></input>
           <input className="register-form-item" type='password' id='password' name='password' placeholder='Password'></input>
-
-          <label for="profile picture">Upload a Profile Picture:</label>
-          <input 
-            className="register-form-item"
-            type='file' 
-            id='profile-picture' 
-            name='profile-picture' 
-            // // onChange={onChange}
-          ></input>
-          
-          <button className="badge badge-primary" >Register</button>
+          <h6 className="upload-img">Upload a Profile Picture:</h6>
+          <input className='register-form-item-img' type="file" name="image-upload" id="image-upload" />
+          <button className="register-btn-submit" >Register</button>
         </form>
+        
+        </div>
     </body>
   );
 }
