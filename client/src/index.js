eec62766ec1be17649a581165aa95157eac1ac63
Mardin Hener <mardinhener14@gmail.com>
2022-09-05 22:21:25 -0400

    Clean up code

--- client/src/index.js
+++ client/src/index.js
@@ -16,6 +16,7 @@ import NewRecipe from './routes/recipes/newRecipe';
 import Recipe from './routes/recipes/recipe';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <BrowserRouter>
     <Routes>
