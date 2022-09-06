d8722873daa47920c471c237616040ed5d78475b
Mardin Hener <mardinhener14@gmail.com>
2022-09-06 16:42:42 -0400

    Setup Bootstrap

--- client/src/routes/recipes/recipeCard.jsx
+++ client/src/routes/recipes/recipeCard.jsx
@@ -1,9 +1,10 @@
+import './recipeCard_style.scss';
+
+import Carousel from 'react-bootstrap/Carousel';
+
 export default function RecipeCard(props) {
   return (
-    <header>
-    {props.mode}
-    <h1>Hello I am a recipe card, hover over me to show different stuff!</h1>
-    <h2>Thank you for hovering!</h2>
-    </header>
+    <>
+    </>
   );  
 }
