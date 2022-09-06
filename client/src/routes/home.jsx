3dcf9a029113191b1bc825fd6fbebab861dcace6
Mardin Hener <mardinhener14@gmail.com>
2022-09-06 15:07:23 -0400

    Create homepage skeleton

--- client/src/routes/home.jsx
+++ client/src/routes/home.jsx
@@ -1,13 +1,19 @@
+import './home.scss';
+
 import RecipeList from './recipes/recipeList'
 
 export default function Home() {
   return (
-    <header>
-      <h1>Get ready to GitCooking!</h1>
-      <RecipeList 
-        mode = "weeklyRecipes"
-      />
-    </header>
-
+    <>
+      <h2 className='home-page-title'>Get ready to GitCooking!</h2>
+        <div className='grouped-recipes'>
+          <h3 className='home-page-subheading'>Top 10 Recipes of the Week!</h3>
+          <RecipeList/>
+          <h3 className='home-page-subheading'>Popular Recipes</h3>
+          <RecipeList/>
+          <h3 className='home-page-subheading'>Most Spooned Recipes</h3>
+          <RecipeList/>
+        </div>
+    </>
   );  
 }
