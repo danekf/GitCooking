59e494cb5389b1681a5844107aae695b15dac23a
Mardin Hener <mardinhener14@gmail.com>
2022-09-07 12:18:24 -0400

    merge recipe-list with main

--- client/src/routes/home.jsx
+++ client/src/routes/home.jsx
@@ -5,14 +5,16 @@ import RecipeList from './recipes/recipeList'
 export default function Home() {
   return (
     <>
-      <h2 className='home-page-title'>Get ready to GitCooking!</h2>
-        <div className='grouped-recipes'>
-          <h3 className='home-page-subheading'>Top 10 Recipes of the Week!</h3>
-          <RecipeList/>
-          <h3 className='home-page-subheading'>Popular Recipes</h3>
-          <RecipeList/>
-          <h3 className='home-page-subheading'>Most Spooned Recipes</h3>
-          <RecipeList/>
+      <div className='home-page-body'>
+        <h2 className='home-page-title'>Get ready to GitCooking!</h2>
+          <div className='grouped-recipes'>
+            <h3 className='home-page-subheading'>Top 10 Recipes of the Week!</h3>
+            <RecipeList/>
+            <h3 className='home-page-subheading'>Popular Recipes</h3>
+            <RecipeList/>
+            <h3 className='home-page-subheading'>Most Spooned Recipes</h3>
+            <RecipeList/>
+          </div>
         </div>
     </>
   );  
