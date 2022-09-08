6ca815351843d07a6b0b81b43e41e91627c26488
Danek <danek.fill@outlook.com>
2022-09-08 17:07:09 -0400

    ADD RECIPE WORKS AFTER BLACK MAGIC

--- server/src/routes/recipes.js
+++ server/src/routes/recipes.js
@@ -18,17 +18,33 @@ module.exports = (db) => {
   });
 
   router.post('/new', (request, response)=>{
-    const {user_id, original_fork_id, title, ingredients, equipment, instructions, recipe_photos, tags, forks, servings } = request.body;
+    const {user_id, original_fork_id, title, recipe_photos, servings } = request.body;
+    console.log(request.body)  
+
+  //black magic to make it work with pg, more greyish white magic actually.
+  const ingredients = JSON.stringify(request.body.ingredients);
+  const equipment = JSON.stringify(request.body.equipment);
+  const instructions = JSON.stringify(request.body.instructions);
+  
+  //black magic to mage an array work when inputting into pg
+  const tags = JSON.stringify(request.body.tags)
+    .replace('[', '{')
+    .replace(']', '}');
+
+
+   console.log('type of: ', typeof tags);
+   console.log(tags);
 
     const queryString = `
     INSERT INTO recipes
-      (user_id, original_fork_id, title, ingredients, equipment, instructions, recipe_photos, tags, forks, servings)
+      (user_id, original_fork_id, title, ingredients, equipment, instructions, recipe_photos, tags, servings)
     VALUES
-    ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+    ($1, $2, $3, $4, $5, $6, $7, $8, $9)
     RETURNING *;`
     ;
 
-    const queryValues = [`${user_id}`, `${original_fork_id}`, `${title}`, `${ingredients}`, `${equipment}`, `${instructions}`, `${recipe_photos}`, `${tags}`, `${forks}`, `${servings}`];
+    const queryValues = [`${user_id}`, `${original_fork_id}`, `${title}`, `${ingredients}`, `${equipment}`, `${instructions}`, `${recipe_photos}`, `${tags}`, `${servings}`];
+    console.log(queryValues)
 
     db.query(queryString, queryValues)
       .then(({ rows: recipe }) =>{
