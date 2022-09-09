29512148cadb724f51518134e8adced3ee350bdb
Danek <danek.fill@outlook.com>
2022-09-09 16:47:32 -0400

    Add image location and text css

--- client/src/routes/recipes/recipeCard.jsx
+++ client/src/routes/recipes/recipeCard.jsx
@@ -13,96 +13,42 @@ export default function RecipeCard(props) {
   const[hover, setHover]= useState(false)
 
   return (
-    <div>
-      <h1>{recipe.title}</h1>
+  //   <div class="container">
+  //     <img src="https://www.w3schools.com/howto/img_snow_wide.jpg" alt="Snow" width="100%"/>
+  //     <div class="bottom-left">Bottom Left</div>
+  //     <div class="top-left">Top Left</div>
+  //     <div class="top-right">Top Right</div>
+  //     <div class="bottom-right">Bottom Right</div>
+  //     <div class="centered">Centered</div>
+  // </div>
+
+
+
+    <div className='container'
+    onMouseEnter={() => setHover(true)}
+    onMouseLeave={() => setHover(false)}
+    >
+      <img src="https://www.w3schools.com/howto/img_snow_wide.jpg" alt="Snow" width="100%"/>
       {!hover && (
-        <div onMouseEnter={() => setHover(true)}>
-          Estimated time : {recipe.estimatedtime}
+        <>
+        <div className='bottom-center' >
+          {recipe.title}
         </div>
+        <div className='bottom-right' >
+          Estimated time : {recipe.estimatedtime} minutes.
+        </div>
+        </>
       )}
 
       {hover && (
-        <div onMouseLeave={() => setHover(false)}>
-        <ul> 
-          <li>Tags: {recipe.tags.map((tag)=> `${tag},`)}</li>
-        </ul>
-      
-      </div>
-      )}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    {/* <div className='recipe-card-body'>
-      <Carousel fade>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/recipe-image-legacy-id-1001451_6-c8d72b8.jpg?resize=960,872?quality=90&webp=true&resize=600,545"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?resize=960,872?quality=90&webp=true&resize=600,545"
-            alt="Second slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/meatball-lasagne-e64e936.jpg?resize=960,872?quality=90&webp=true&resize=600,545"
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-      </div> */}
-
+        <>
+        <div classname='center'>
+        <div className='bottom-left' >
+          Tags : {recipe.tags}
+        </div>
+        </div>
+      </>
+      )}    
     </div>
   );  
 }
