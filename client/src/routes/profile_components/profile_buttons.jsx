ef8d473e282bbce785fca63b7f1da29bb9249645
Mardin Hener <mardinhener14@gmail.com>
2022-09-09 12:47:56 -0400

    Add conditional rendering on profile favourites/report button

--- client/src/routes/profile_components/profile_buttons.jsx
+++ client/src/routes/profile_components/profile_buttons.jsx
@@ -1,9 +1,13 @@
-import { Link } from "react-router-dom";
-const [user, setUser] = useState(false)
+import { useLocation, Link } from "react-router-dom";
 
 export default function ProfileButtons () {
+  const location = useLocation();
+  const user = location.state?.user
+
   return (
     <>
+    {/* Conditional Rendering, if a user is logged in show the favourites button, if a user is not logged in show the report button */}
+    {!user ? 
       <div>
         <button className="message-button">
           Message
@@ -12,6 +16,18 @@ export default function ProfileButtons () {
           Report
         </button>
       </div>
+    :
+      <div>
+        <button className="message-button">
+          Message
+        </button>
+        <Link to='/favourites' className="link-favourites">
+        <button className="favourites-button">
+          Favourites
+        </button>
+        </Link>
+      </div>
+    }
     </>
   );
 }
