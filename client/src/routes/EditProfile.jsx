5deac354444e49b3dd3b6b14bcd52cb720634f2f
Mardin Hener <mardinhener14@gmail.com>
2022-09-09 14:06:29 -0400

    Create Edit Profile Skelton

--- client/src/routes/EditProfile.jsx
+++ client/src/routes/EditProfile.jsx
@@ -2,7 +2,38 @@ import React from 'react'
 
 export default function EditProfile() {
   return (
-<>
-</>  
-)
+    <main>
+       <form className="edit-form" /* onSubmit={something happens here}*/>
+          <input className="edit-form-item" type='text' id='first_name' name='first_name' placeholder='First Name'></input>
+          <input className="edit-form-item" type='text' id='last_name' name='last_name' placeholder='Last Name'></input>
+          <input className="edit-form-item" type='password' id='password' name='password' placeholder='Password'></input>
+          <input className="edit-form-item" type="text" name="socials" id="socials">
+            <div className="socials-list-item">
+              <ul>
+                <li>
+                  <input className="edit-form-item" type="URL" id='URL' name='URL' placeholder='Instagram' /><i class="fa-brands fa-instagram"></i> 
+                </li>
+                <li>
+                  <input className="edit-form-item" type="URL" id='URL' name='URL' placeholder='YouTube' /><i class="fa-brands fa-youtube"></i>
+                </li>
+                <li>
+                  <input className="edit-form-item" type="URL" id='URL' name='URL' placeholder='TikTok' /><i class="fa-brands fa-tiktok"></i>
+                </li>
+                <li>
+                  <input className="edit-form-item" type="URL" id='URL' name='URL' placeholder='Twitter' /><i class="fa-brands fa-twitter"></i>
+                </li>
+                <li>
+                  <input className="edit-form-item" type="URL" id='URL' name='URL' placeholder='LinkedIn' /><i class="fa-brands fa-linkedin"></i>
+                </li>
+              </ul>
+            </div>
+          </input>
+          <input className="edit-form-item" type="text" name="qualifications" id="qualifications" />
+          <input className="edit-form-item" type="text" name="bio" id="bio" />
+          <h6 className="upload-img">Change a Profile Picture:</h6>
+          <input className='edit-form-item-img' type="file" name="image-upload" id="image-upload" />
+          <button className="edit-btn-submit" type='submit' >Save Changes</button>
+        </form>
+      </main>
+  );
 }
