c56fba46ed1b13f68583420237f019d36115a0b2
KayliedeBlois <kayliedeblois@gmail.com>
2022-09-10 22:05:40 -0400

    Update carousel arrows

--- client/src/routes/recipeCarousel.jsx
+++ client/src/routes/recipeCarousel.jsx
@@ -3,11 +3,30 @@ import Carousel from 'react-bootstrap/Carousel';
 import CarouselItem from "react-bootstrap/esm/CarouselItem";
 
 
+
 export default function RecipeCarousel(props) {
   const recipes = props.recipes
+
+  const directionButtons = (direction) => {
+    return (
+      <span
+        aria-hidden="true"
+        className={direction === "Next" ? "button-next" : "button-prev"}
+      >
+        {direction}
+      </span>
+    );
+  };
+
   return (
     <>
-    <Carousel fade>
+    <Carousel 
+      fade
+      nextLabel={"Next"}
+      prevLabel={"Previous"}
+      nextIcon={directionButtons(<i class="fa-solid fa-angles-right"></i>)}
+      prevIcon={directionButtons(<i class="fa-solid fa-angles-left"></i>)}
+    >
       {recipes.map((recipe) => 
         <CarouselItem>
           <RecipeCard recipe={recipe}/>          
