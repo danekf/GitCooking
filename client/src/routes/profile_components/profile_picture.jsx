ee64388aee137f55d275a2a78c08b85fd0684034
KayliedeBlois <kayliedeblois@gmail.com>
2022-09-13 23:10:26 -0400

    Update background colours

--- client/src/routes/profile_components/profile_picture.jsx
+++ client/src/routes/profile_components/profile_picture.jsx
@@ -1,6 +1,6 @@
 import './styles.scss'
-
 export default function ProfilePicture (props) {
+
   return (
     <div> 
       <img src={props.profile_picture} width='80px' className="profile-picture"/>
