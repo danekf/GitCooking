248a6d626149e90d9fd56b7621c1bd1cbccc71b4
Danek <danek.fill@outlook.com>
2022-09-14 12:01:32 -0400

    Remove tagsd from original recipes but add them back

--- server/src/db/seeds/02_recipes.sql
+++ server/src/db/seeds/02_recipes.sql
@@ -1,160 +1,160 @@
--- INSERT INTO recipes (user_id, original_fork_id, title, ingredients, equipment, instructions, estimatedTime, recipe_photos, tags, servings, forks)
--- VALUES(
---   1,
---   0,
---   'Pasta',
---   '[
---     { 
---       "ingredientName":"pasta", 
---       "ingredientQty":"1"
---     },
---     {
---       "ingredientName":"TBsp pesto", 
---       "ingredientQty":"3" 
---     },
---     {
---       "ingredientName":"tbsp salt", 
---       "ingredientQty":"1"
---     }
---   ]',
---   '[{"equipmentName": "Pot", "equipmentQty": "1"}]',
---   '[
---     {
---       "estimatedTime": 1, 
---       "instruction": "Add salt to water so that water tastes distinctly salted, This is to flavour the pasta, NOT to make the water boil faster. It is essential to tasty pasta."
---     },
---     {
---       "estimatedTime": 5,
---        "instruction": "Boil water"
---     },
---     {
---       "estimatedTime": 7,
---       "instruction": "Cook the pasta in the water"
---     },
---     {
---       "estimatedTime": 2,
---       "instruction": "Drain Pasta and add pesto."
---     },
---     {
---       "estimatedTime": 1,
---       "instruction": "Serve."
---     }
---   ]',
---   16,
---   '/recipePhotos/Default.jpg',
---   ARRAY ['Pasta', 'Simple', 'Quick', 'Easy', 'Budget', 'top', 'vegetarian'],
---   4,
---   ARRAY [2]
--- );
+INSERT INTO recipes (user_id, original_fork_id, title, ingredients, equipment, instructions, estimatedTime, recipe_photos, tags, servings, forks)
+VALUES(
+  1,
+  0,
+  'Pasta',
+  '[
+    { 
+      "ingredientName":"pasta", 
+      "ingredientQty":"1"
+    },
+    {
+      "ingredientName":"TBsp pesto", 
+      "ingredientQty":"3" 
+    },
+    {
+      "ingredientName":"tbsp salt", 
+      "ingredientQty":"1"
+    }
+  ]',
+  '[{"equipmentName": "Pot", "equipmentQty": "1"}]',
+  '[
+    {
+      "estimatedTime": 1, 
+      "instruction": "Add salt to water so that water tastes distinctly salted, This is to flavour the pasta, NOT to make the water boil faster. It is essential to tasty pasta."
+    },
+    {
+      "estimatedTime": 5,
+       "instruction": "Boil water"
+    },
+    {
+      "estimatedTime": 7,
+      "instruction": "Cook the pasta in the water"
+    },
+    {
+      "estimatedTime": 2,
+      "instruction": "Drain Pasta and add pesto."
+    },
+    {
+      "estimatedTime": 1,
+      "instruction": "Serve."
+    }
+  ]',
+  16,
+  '/recipePhotos/Default.jpg',
+  ARRAY ['Pasta', 'Simple', 'Quick', 'Easy', 'Budget'],
+  4,
+  ARRAY [2]
+);
 
 
--- --fork of recipe 1, which changed pesto for tomato sauce and added meatballs.
--- INSERT INTO recipes (user_id, original_fork_id, title, ingredients, equipment, instructions, estimatedTime, recipe_photos, tags, servings, forks)
--- VALUES(
---   1,
---   1, --forked from 1, so id of 1 referencing the original fork id. Client will provide number when needed and handle logic to reference it.
---   'Pasta with meatballs',
---   '[
---     {
---       "ingredientName":"handful pasta",
---      "ingredientQty":"1 "
---     },
---     {
---       "ingredientName":"TBsppesto",
---       "ingredientQty":"3"
---     },
---     {
---       "ingredientName":"TBsp salt", 
---       "ingredientQty":"1"
---     },
---     {
---       "ingredientName":"Meatballs",
---       "ingredientQty":"5"
---     }
---   ]',
---   '[{"equipmentName": "Pot", "equipmentQty": "1"}]',
---   '[
---     {
---       "estimatedTime": 1,
---       "instruction": "Add salt to water so that water tastes distinctly salted, This is to flavour the pasta, NOT to make the water boil faster. It is essential to tasty pasta."
---     },
---     {
---       "estimatedTime": 5,
---       "instruction": "Boil water"
---     },
---     {
---       "estimatedTime": 7,
---       "instruction": "Cook the pasta in the water"
---       },
---     {
---       "estimatedTime": 7,
---       "instruction": "While pasta is cooking, heat up pasta sauce and add in meatballs to heat through."
---     },
---     {
---       "estimatedTime": 2,
---       "instruction": "Drain Pasta and add sauce + meatballs."
---     },
---     {
---       "estimatedTime": 1,
---       "instruction": "Serve."
---     }
---   ]',
---   23,
---   '/recipePhotos/Default.jpg',
---   ARRAY ['Pasta', 'Simple', 'Quick', 'Easy', 'Budget', 'Meatballs', 'Tomato Sauce', 'top', 'meat'],
---   4,
---   null
--- );
+--fork of recipe 1, which changed pesto for tomato sauce and added meatballs.
+INSERT INTO recipes (user_id, original_fork_id, title, ingredients, equipment, instructions, estimatedTime, recipe_photos, tags, servings, forks)
+VALUES(
+  1,
+  1, --forked from 1, so id of 1 referencing the original fork id. Client will provide number when needed and handle logic to reference it.
+  'Pasta with meatballs',
+  '[
+    {
+      "ingredientName":"handful pasta",
+     "ingredientQty":"1 "
+    },
+    {
+      "ingredientName":"TBsppesto",
+      "ingredientQty":"3"
+    },
+    {
+      "ingredientName":"TBsp salt", 
+      "ingredientQty":"1"
+    },
+    {
+      "ingredientName":"Meatballs",
+      "ingredientQty":"5"
+    }
+  ]',
+  '[{"equipmentName": "Pot", "equipmentQty": "1"}]',
+  '[
+    {
+      "estimatedTime": 1,
+      "instruction": "Add salt to water so that water tastes distinctly salted, This is to flavour the pasta, NOT to make the water boil faster. It is essential to tasty pasta."
+    },
+    {
+      "estimatedTime": 5,
+      "instruction": "Boil water"
+    },
+    {
+      "estimatedTime": 7,
+      "instruction": "Cook the pasta in the water"
+      },
+    {
+      "estimatedTime": 7,
+      "instruction": "While pasta is cooking, heat up pasta sauce and add in meatballs to heat through."
+    },
+    {
+      "estimatedTime": 2,
+      "instruction": "Drain Pasta and add sauce + meatballs."
+    },
+    {
+      "estimatedTime": 1,
+      "instruction": "Serve."
+    }
+  ]',
+  23,
+  '/recipePhotos/Default.jpg',
+  ARRAY ['Pasta', 'Simple', 'Quick', 'Easy', 'Budget', 'Meatballs', 'Tomato Sauce'],
+  4,
+  null
+);
 
--- INSERT INTO recipes (user_id, original_fork_id, title, ingredients, equipment, instructions, estimatedTime, recipe_photos, tags, servings, forks)
--- VALUES(
---   2,
---   0,
---   'Water',
---   '[
---     {
---       "ingredientName":"water", 
---       "ingredientQty":"1"
---     }    
---   ]',
---   '[{"equipmentName": "Cup", "equipmentQty": "1"}]',
---   '[
---     {
---       "estimatedTime": 1, 
---       "instruction": "Add water to cup"
---     }
---   ]',
---   1,
---   '/recipePhotos/Default.jpg',
---   ARRAY ['water', 'top'],
---   1,
---   null
--- );
+INSERT INTO recipes (user_id, original_fork_id, title, ingredients, equipment, instructions, estimatedTime, recipe_photos, tags, servings, forks)
+VALUES(
+  2,
+  0,
+  'Water',
+  '[
+    {
+      "ingredientName":"water", 
+      "ingredientQty":"1"
+    }    
+  ]',
+  '[{"equipmentName": "Cup", "equipmentQty": "1"}]',
+  '[
+    {
+      "estimatedTime": 1, 
+      "instruction": "Add water to cup"
+    }
+  ]',
+  1,
+  '/recipePhotos/Default.jpg',
+  ARRAY ['water', 'top'],
+  1,
+  null
+);
 
--- INSERT INTO recipes (user_id, original_fork_id, title, ingredients, equipment, instructions, estimatedTime, recipe_photos, tags, servings, forks)
--- VALUES(
---   2,
---   0,
---   'Water++',
---   '[
---     {
---       "ingredientName":"water", 
---       "ingredientQty":"2"
---     }    
---   ]',
---   '[{"equipmentName": "Cup", "equipmentQty": "2"}]',
---   '[
---     {
---       "estimatedTime": 1, 
---       "instruction": "Add water to cup"
---     }
---   ]',
---   1,
---   '/recipePhotos/Default.jpg',
---   ARRAY ['water'],
---   2,
---   null
--- );
+INSERT INTO recipes (user_id, original_fork_id, title, ingredients, equipment, instructions, estimatedTime, recipe_photos, tags, servings, forks)
+VALUES(
+  2,
+  0,
+  'Water++',
+  '[
+    {
+      "ingredientName":"water", 
+      "ingredientQty":"2"
+    }    
+  ]',
+  '[{"equipmentName": "Cup", "equipmentQty": "2"}]',
+  '[
+    {
+      "estimatedTime": 1, 
+      "instruction": "Add water to cup"
+    }
+  ]',
+  1,
+  '/recipePhotos/Default.jpg',
+  ARRAY ['water'],
+  2,
+  null
+);
 
 -- INSERT INTO recipes (user_id, original_fork_id, title, ingredients, equipment, instructions, estimatedTime, recipe_photos, tags, servings, forks)
 -- VALUES(
